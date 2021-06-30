1df3aa8029fceef2ec40efa66f54b57ff4026ddc
root <srirammahesh16@gmail.com>
2021-06-30 20:15:53 +0000

    updated 3rd time

--- roles/apache2/index.php
+++
@@ -1,7 +0,0 @@
-<pre>
-<?php
-print("Hello World! The Below Results shows the ifconfig information.  \n");
-print("NOTE: The IP address changes as you change hit the refresh button \n\n\n");
-print `/usr/sbin/ifconfig`;
-?>
-</pre>
